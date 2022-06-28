220c8c3d5225a3b5711d4db476c8d1f11fdcf14b
dearlk <34156807+dearlk@users.noreply.github.com>
2022-06-28 16:51:06 +0800

    initial commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,27 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifiedtodo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifiedtodof698af03": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "analytics": {
+        "amplifiedtodo": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
